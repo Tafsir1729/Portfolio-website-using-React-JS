cba296dbbd65d81e7abaae8099b1749914db447d
Tafsir Ahamed <63195907+Tafsir1729@users.noreply.github.com>
2020-11-03 18:02:29 +0600

    03/11/2020

---
+++ user/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopBanner from "./components/TopBanner/TopBanner";
+
+
+function App() {
+  return (
+    <div>
+
+      <TopBanner></TopBanner>
+
+    </div>
+  );
+}
+
+export default App;
